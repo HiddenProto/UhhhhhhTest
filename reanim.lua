0c0e73313bcfa281a62382482d36e014449be5b1
MethE <66harpseal66@gmail.com>
2026-06-27 11:34:25 -0400

    sync: edited reanim.lua 2026-06-27_11-34-25

--- reanim.lua
+++ reanim.lua
@@ -7446,7 +7446,7 @@ do
 	end)
 	local NetVelSlider = UI.CreateSlider(MainPage, "Netless Velocity", Reanimate.NetlessVelocity, 25.01, 1000, 0.1)
 	NetVelSlider.Changed:Connect(function(val)
-		val = math.clamp(val, 25.01, 30)
+		val = math.clamp(val, 25.01, 1000)
 		NetVelSlider.Value = val
 		Reanimate.NetlessVelocity = val
 		SaveData.NetlessVelocity = val
